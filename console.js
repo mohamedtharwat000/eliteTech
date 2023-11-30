8323a42d48d3b7535a0e369d42b7ede102acda02
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-30 21:49:42 +0200

    Update console.js

--- console.js
+++ console.js
@@ -1,11 +1,28 @@
-import { program, command } from 'bandersnatch';
+// import { program, command } from 'bandersnatch';
+// import data from './dataManager.js';
+
+// const cmd = command()
+//   .default()
+//   .description('Outputs "bar".')
+//   .action(() => console.log('bar'));
+
+// const app = program().description('foo').add(cmd);
+
+// app.run();
+
 import data from './dataManager.js';
 
-const cmd = command()
-  .default()
-  .description('Outputs "bar".')
-  .action(() => console.log('bar'));
+// put all data needed to creat and save to database new product
+console.log(await data.create('mice', { name: '', type: '' }));
 
-const app = program().description('foo').add(cmd);
+// update product with id = 1 with new data
+console.log(await data.update('mice', { id: 1, name: 'new name' }));
 
-app.run();
+// delete product with id = 1
+console.log(await data.delete('mice', { id: 1 }));
+
+// get product data with id = 1
+console.log(await data.read('mice', { id: 1 }));
+
+// get all products data
+console.log(await data.read('mice', {}));
