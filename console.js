c7873bd591baa0804098efe1bad7ee37a6f2f203
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2023-11-30 22:35:14 +0200

    Update console.js

--- console.js
+++ console.js
@@ -9,20 +9,3 @@
 // const app = program().description('foo').add(cmd);
 
 // app.run();
-
-import data from './dataManager.js';
-
-// put all data needed to creat and save to database new product
-console.log(await data.create('mice', { name: '', type: '' }));
-
-// update product with id = 1 with new data
-console.log(await data.update('mice', { id: 1, name: 'new name' }));
-
-// delete product with id = 1
-console.log(await data.delete('mice', { id: 1 }));
-
-// get product data with id = 1
-console.log(await data.read('mice', { id: 1 }));
-
-// get all products data
-console.log(await data.read('mice', {}));
