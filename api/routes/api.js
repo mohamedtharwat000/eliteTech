2fd5e462ca225a4f1c18acb54066403c02f4f712
LORDyyyyy <mlord6271@gmail.com>
2023-12-11 17:33:43 +0200

    updated api.html api.js

--- api/routes/api.js
+++ api/routes/api.js
@@ -35,7 +35,22 @@ router
 
 router.get("/:type/", logger, async (req, res) => {
 	let type = req.params.type;
-	let result = await data.read(type, req.query);
+	let query = req.query;
+
+	if (query.hasOwnProperty("limit")) {
+		let limit = query["limit"];
+
+		if (/^\d+$/.test(limit)) {
+			query["limit"] = limit;
+		}
+		else {
+			delete query["limit"];
+			delete query["start"];
+			delete query["end"];
+			query = { ...getSequnece(limit), ...query };
+		}
+	}
+	let result = await data.read(type, query);
 	res.json(result);
 });
 
@@ -48,13 +63,6 @@ router.post("/:type/", logger, async (req, res) => {
 });
 
 
-router.get("/:type/limit/:sequence", sequenceLogger, async (req, res) => {
-	let type = req.params.type;
-	let result = await data.read(type, getSequnece(req.params.sequence));
-	res.json(result);
-});
-
-
 /**
  * A logger function for the /api/type/id and /api/type route
  *
@@ -64,7 +72,6 @@ router.get("/:type/limit/:sequence", sequenceLogger, async (req, res) => {
  */
 function logger(req, res, next) {
 	console.log(req.originalUrl);
-	// console.log(req.params);
 	if (!types.includes(req.params.type)) {
 		res.status(404).json({ 'error': 'type not found' });
 	} else {
@@ -72,62 +79,22 @@ function logger(req, res, next) {
 	}
 }
 
-
-/**
- * A logger function for the /api/type/limit/sequence route
- *
- * @param req - the request
- * @param res - the resopond
- * @param next - the next function to be called
- */
-function sequenceLogger(req, res, next) {
-	console.log(req.originalUrl);
-	if (!types.includes(req.params.type)) {
-		res.status(404).json({ 'error': 'type not found' });
-	} else if (checkSequence(req.params.sequence)) {
-		let message = { 'error': 'wrong limitation usage', 'usage': ['start:n:n', 'n:end:n', 'n:n:limit', 'start:end:n', 'start:n:limit'] }
-		res.status(404).json(message);
-	} else {
-		next();
-	}
-}
-
-
-/**
- * Check if the request parameter "sequence" is a valid sequence
- *
- * @param {string} sequence - the request parameter, sequence
- * @returns {int} 0 if valid, 1 otherwise
- */
-function checkSequence(sequence) {
-	sequence = sequence.split(':');
-	if (sequence.length != 3) {
-		return (1);
-	} else if (!sequence.every(part => /^\d+$|^n$/i.test(part))) {
-		return (1);
-	}
-	let regexSequence = /^\d+nn$|^n\d+n$|^nn\d+$|^\d+\d+n$|^\d+n\d+$/i;
-	let validSequence = regexSequence.test(sequence.join(''));
-	if (!validSequence)
-		return (1);
-	return (0);
-}
 
 
 /**
  * Get the right sequence to apply it when making limitation
  *
  * @param {string} sequence - the request parameter, sequence
- * @returns {dict} a dictitnory that contatons the right value for the start, end, count 
+ * @returns {dict} a dictionary that contains the right value for the start, end, and limit.
  */
 function getSequnece(sequence) {
 
 	let values = sequence.split(':');
-	console.log(sequence);
+
 	sequence = values.join('');
 	values = values.map((e) => parseInt(e));
 	switch (true) {
-		case /^\d*nn$/i.test(sequence):
+		case /^\d+nn$/i.test(sequence):
 			return ({ start: values[0] });
 		case /^n\d+n$/i.test(sequence):
 			return ({ end: values[1] });
